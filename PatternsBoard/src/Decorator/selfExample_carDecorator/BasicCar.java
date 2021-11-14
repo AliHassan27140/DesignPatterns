5b874cf88fc48439735320261d57d9f8bd782385
AliH27140 <alihassanhrp0@gmail.com>
2021-11-14 13:43:52 +0500

    Add an example of Decorator pattern

---
+++ PatternsBoard/src/Decorator/selfExample_carDecorator/BasicCar.java
@@ -0,0 +1,10 @@
+package Decorator.selfExample_carDecorator;
+
+public class BasicCar implements Car {
+
+	@Override
+	public void drive() {
+		System.out.print("Basic Car Drives");
+	}
+
+}
---
+++ PatternsBoard/src/Decorator/selfExample_carDecorator/Car.java
@@ -0,0 +1,6 @@
+package Decorator.selfExample_carDecorator;
+
+public interface Car {
+
+	void drive();
+}
